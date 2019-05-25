b0e927caecaa07ad8c855171c0f4eb904fe89aea
Turly O'Connor <turlyo@gmail.com>
2019-05-25 23:50:05 +0200

    Strip unwanted audio. Sonarr calls fix-mkv-langs.

--- sonarr-add-aac-audio.sh
+++ sonarr-add-aac-audio.sh
@@ -1,26 +1,32 @@
 #!/bin/bash
 #
 # sonarr/radarr on download video with non AAC first track, add one and make it the default.
-# See the add-aac-audio script in this directory.
+# fix any "language=und" undefined language in metadata
+# See the add-aac-audio and fix-mkv-langs scripts in this directory.
 #
 # turly 2019
 
 WHEREAMI="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 AAAC="$WHEREAMI/add-aac-audio"      # add-aac-audio script in same dir as this one
+FIXLANGS="$WHEREAMI/fix-mkv-langs"  # Fix "language=und" metadata for audio/subtitles etc
 LOGFILE=$WHEREAMI/saac.log
 
 >>$LOGFILE echo `date`: sonarr-add-aac-audio.sh - $WHEREAMI - event type $sonarr_eventtype $radarr_eventtype
 
 if [ "$sonarr_eventtype" == "Download" ]; then
-    >>$LOGFILE echo `date`: "${AAAC}" "$sonarr_episodefile_path" --overwrite
-    if ! "${AAAC}" --quiet "$sonarr_episodefile_path" --overwrite &>> $LOGFILE; then
+    >>$LOGFILE echo `date`: "${FIXLANGS}" "$sonarr_episodefile_path"
+    "${FIXLANGS}" "$sonarr_episodefile_path" &>> $LOGFILE
+    >>$LOGFILE echo `date`: "${AAAC}" "$sonarr_episodefile_path"
+    if ! "${AAAC}" "$sonarr_episodefile_path" &>> $LOGFILE; then
         >&2 echo "Failed to add AAC audio to" $sonarr_episodefile_path
         >>$LOGFILE echo "Failed to add AAC audio to" $sonarr_episodefile_path
         exit 1
     fi
 elif [ "$radarr_eventtype" == "Download" ]; then
-    >>$LOGFILE echo `date`: "${AAAC}" "$radarr_moviefile_path" --overwrite
-    if ! "${AAAC}" --quiet "$radarr_moviefile_path" --overwrite &> /dev/null; then
+    >>$LOGFILE echo `date`: "${FIXLANGS}" "$radarr_moviefile_path"
+    "${FIXLANGS}" "$radarr_episodefile_path" &>> $LOGFILE
+    >>$LOGFILE echo `date`: "${AAAC}" "$radarr_moviefile_path"
+    if ! "${AAAC}" "$radarr_moviefile_path" &> /dev/null; then
         >&2 echo "Failed to add AAC audio to" $radarr_moviefile_path
         >>$LOGFILE echo "Failed to add AAC audio to" $radarr_moviefile_path
         exit 1
